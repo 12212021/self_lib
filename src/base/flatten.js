6dc7bbc2dafe757b9ff17800e9db642d4937cdd1
yujianghua <yujianghua@baidu.com>
2021-04-15 10:10:55 +0800

    flat update

--- src/base/flatten.js
+++ src/base/flatten.js
@@ -17,4 +17,37 @@ function flat(array, deep = 1) {
     }, [])
 }
 
-export {flat};
+/**
+ * 
+ * @param {Array} array 
+ * @param {Number} deep 
+ * @returns {Array}
+ */
+function flatIter(array, deep = 1) {
+    let count = -1;
+    let copy = array.slice();
+    let result = [];
+    // 单调性体现
+    while(++count < deep) {
+        let isFlatted = true;
+        let index = -1;
+        result = [];
+        while(++index < copy.length) {
+            const val = copy[index];
+            if (Array.isArray(val)) {
+                // 单调性体现
+                result = result.concat(val);
+                isFlatted = false;
+            } else {
+                result.push(val);
+            }
+        }
+        copy = result.slice();
+        if (isFlatted) {
+            return result;
+        }
+    }
+    return result;
+}
+
+export {flat, flatIter};
