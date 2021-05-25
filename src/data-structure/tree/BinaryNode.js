f3eb5ad47a3dbfa1db40694a8c828659c33447c9
12212021 <12212021@bjtu.edu.cn>
2021-05-25 10:33:38 +0800

    二叉树部分实现

---
+++ src/data-structure/tree/BinaryNode.js
@@ -0,0 +1,213 @@
+import {randomInt} from '../utils'
+
+
+/**
+ * 树等价于连通无环图，由一组顶点（vertex）和若干连接的边组成（edge），可以指定某一个特定的节点为根节点
+ *
+ * 约定 沿着节点（v）到根节点（r）的唯一通路经过的边的数目成为v的深度，记作depth(v)， depth(root) = 0
+ *
+ * 当且仅当v节点为根节点的时候  v.parent == NULL;
+ * 当且仅当v.lChild == NULL && v.rChild == NULL，v为叶子节点
+ *
+ *
+ */
+class BinaryNode {
+    constructor(e, parent) {
+        this.data = e;
+        this.parent = parent;
+        this.lChild = undefined;
+        this.rChild = undefined;
+        // 数的高度
+        this.height = undefined;
+        // Null Path Length （左式堆）
+        this.npl = undefined;
+        this.color = undefined;
+    }
+
+    size() {}
+
+    insertAsLC(e) {
+        this.lChild = new BinaryNode(e, this);
+    }
+
+    insertAsRC(e) {
+        this.rChild = new BinaryNode(e, this);
+    }
+
+    succ() {}
+
+    /**
+     * 子树层次遍历
+     */
+    travLevel() {}
+
+    /**
+     * 子树先序遍历
+     */
+    travPre() {}
+
+    /**
+     * 子树中序遍历
+     */
+    travIn_I1() {}
+
+    /**
+     * 子树中序遍历
+     */
+    travIn_I2() {}
+
+    /**
+     * 子树中序遍历
+     */
+    travIn_I3() {}
+
+    /**
+     * 子树中序遍历
+     */
+    travIn_I4() {}
+
+    /**
+     * 子树中序遍历
+     */
+    travIn_R() {}
+
+    /**
+     * 子树后序遍历
+     */
+    travPost() {}
+
+    /**
+     * 中序遍历算法的统一入口
+     * @param {Function} callback
+     */
+    travIn(callback) {
+        switch (randomInt(5) % 5) {
+            case 0:
+                this.travIn_I1(this, callback); // 迭代1
+                break;
+            case 1:
+                this.travIn_I2(this, callback); // 迭代2
+                break;
+            case 2:
+                this.travIn_I3(this, callback); // 迭代3
+                break;
+            case 3:
+                this.travIn_I4(this, callback); // 迭代4
+                break;
+            case 4:
+                this.travIn_R(this, callback); // 递归版
+                break;
+        }
+    }
+}
+
+/**
+ *
+ * @param {BinaryNode} v
+ * @returns {Boolean}
+ */
+function IsRoot(v) {
+    return v.parent === null;
+}
+
+/**
+ *
+ * @param {BinaryNode} v
+ * @returns {Boolean}
+ */
+function isLChild(v) {
+    return !IsRoot(v) && v.parent.lChild === v;
+}
+
+/**
+ *
+ * @param {BinaryNode} v
+ * @returns {Boolean}
+ */
+function isRChild(v) {
+    return !IsRoot(v) && v.paren.rChild === v;
+}
+
+/**
+ *
+ * @param {BinaryNode} v
+ * @returns {Boolean}
+ */
+function hasLChild(v) {
+    return v.lChild !== null;
+}
+
+/**
+ *
+ * @param {BinaryNode} v
+ * @returns {Boolean}
+ */
+function hasRChild(v) {
+    return v.rChild !== null;
+}
+
+/**
+ *
+ * @param {BinaryNode} v
+ * @returns {Boolean}
+ */
+function hasChild(v) {
+    return hasLChild(v) || hasRChild(v);
+}
+
+/**
+ *
+ * @param {BinaryNode} v
+ * @returns {Boolean}
+ */
+function hasBothChild(v) {
+    return hasLChild(v) && hasRChild(v);
+}
+
+/**
+ *
+ * @param {BinaryNode} v
+ * @returns {Boolean}
+ */
+function isLeaf(v) {
+    return !hasChild(v);
+}
+
+/**
+ *
+ * @param {BinaryNode} v
+ * @returns {Boolean}
+ */
+function hasParent(v) {
+    return !IsRoot(v);
+}
+
+/**
+ * 返回p节点的兄弟节点
+ *
+ * @param {BinaryNode} p
+ * @returns {BinaryNode}
+ */
+function sibling(p) {
+    return isLChild(p) ? p.parent.rChild : p.parent.lChild;
+}
+
+/**
+ *
+ * @param {BinaryNode} p
+ * @returns {BinaryNode}
+ */
+function uncle(p) {
+    return (sibling(p.parent));
+}
+
+function fromParentTo(p) {
+    if (IsRoot(p)) {
+        return p;
+    }
+
+    return isLChild(p) ? p.parent.lChild : p.parent.rChild;
+}
+
+
+export {BinaryNode, fromParentTo};
---
+++ src/data-structure/tree/BinaryTree.js
@@ -0,0 +1,204 @@
+import {BinaryNode, fromParentTo} from './BinaryNode'
+
+class BinaryTree {
+    constructor() {
+        this._size = 0;
+        this._root = undefined;
+    }
+    /**
+     * 更新节点的高度
+     *
+     * 实际情况中节点很难发现后代节点发生变化，所以返回来用如下策略来更新：
+     * 一旦有节点加入或者离开树，则更新该节点的所有祖先节点的高度，二者是等效的
+     * @param {BinaryNode} x
+     */
+    updateHeight(x) {
+        const lTreeHeight = x.lChild.height;
+        const rTreeHeight = x.rChild.height;
+        x.height = 1 + Math.max(lTreeHeight, rTreeHeight);
+        return x.height;
+    }
+
+    /**
+     * 更新x节点以及其祖先节点的高度
+     * @param {BinaryNode} x
+     */
+    updateHeightAbove(x) {
+        this.updateHeight(x);
+        let parent = x.parent;
+        while (parent) {
+            /**
+             * 一旦发现再逐层向上更新的过程中，祖先节点高度未发生变化，则可以终止
+             * 就渐进意义而言，算法的复杂度并没有下降数量级
+             */
+            const originHeight = parent.height;
+            const newHeight = this.updateHeight(parent);
+            if (originHeight === newHeight) {
+                break;
+            }
+            parent = parent.parent;
+        }
+    }
+
+    size() {
+        return this._size;
+    }
+
+    empty() {
+        return !this._root;
+    }
+
+    root() {
+        return this._root;
+    }
+
+    insertAsRoot(e) {
+        this._size = 1;
+        this._root = new BinaryNode(e, null);
+        return this._root;
+    }
+
+    /**
+     * 将e作为左孩子插入
+     * @param {BinaryNode} x 父亲节点
+     * @param {*} e 待插入节点的数据
+     */
+    insertAsLC(x, e) {
+        this._size++;
+        x.insertAsLC(e);
+        this.updateHeightAbove(x);
+        return x.lChild;
+    }
+
+    /**
+     * 将e作为右孩子插入
+     * @param {BinaryNode} x
+     * @param {*} e
+     * @returns
+     */
+    insertAsRC(x, e) {
+        this._size++;
+        x.insertAsRC(e);
+        this.updateHeightAbove(x);
+        return x.rChild;
+    }
+
+    /**
+     * 将tree参数代表的树作为x节点的左子树接入，tree置空
+     * @param {BinaryNode} x
+     * @param {BinaryTree} tree
+     */
+    attachAsLC(x, tree) {
+        this._size += tree.size();
+
+        const root = tree.root();
+        root.parent = x;
+        x.lChild = root;
+        this.updateHeightAbove(x);
+
+        tree = null;
+
+        return x;
+    }
+
+    attachAsRC(x, tree) {
+        this._size += tree.size();
+
+        const root = tree.root();
+        root.parent = x;
+        x.rChild = root;
+        this.updateHeightAbove(x);
+
+        tree = null;
+
+        return x;
+    }
+    /**
+     * 删除合法位置的x节点以及其后端，返回被删除节点的数目
+     * @param {BinaryNode} x
+     */
+    remove(x) {
+        fromParentTo(x) = null;
+        this.updateHeightAbove(x.parent);
+        const size = this.removeAt(x);
+        this._size -= size;
+        return size;
+    }
+
+    /**
+     * 递归地移除节点
+     * @param {BinaryNode} x
+     */
+    removeAt(x) {
+        if (!x) return 0;
+        let size = 1 + this.removeAt(x.lChild) + this.removeAt(x.rChild);
+        x = null;
+        return size;
+    }
+
+    /**
+     * 删除x节点以及其子树，并将该子树重新封装，返回给调用者
+     * @param {BinaryNode} x
+     * @returns {BinaryTree}
+     */
+    secede(x) {
+        fromParentTo(x) = null;
+        this.updateHeightAbove(x.parent);
+
+        const newTree = new BinaryTree();
+        newTree._root = x;
+        x.parent = null;
+        newTree._size = x.size();
+
+        this._size -= newTree.size();
+        return newTree;
+    }
+
+    travLevel(callback) {}
+
+    /**
+     * 定义节点以及其左右孩子表示为V、L、R，以V节点访问顺序而言，有先序、中序、后续遍历方式
+     * 先序遍历：VLR
+     * @param {BinaryNode} x
+     * @param {Function} callback
+     */
+    travPre(x, callback) {
+        if (!x) {
+            return;
+        }
+        callback(x);
+        this.travPre(x.lChild, callback);
+        this.travPre(x.rChild, callback);
+    }
+
+    /**
+     * 中序遍历：LVR
+     * @param {BinaryNode} x
+     * @param {Function} callback
+     */
+    travIn(x, callback) {
+        if (!x) {
+            return;
+        }
+        this.travIn(x.lChild, callback);
+        callback(x);
+        this.travIn(x.rChild, callback);
+    }
+
+    /**
+     * 后序遍历：LRV
+     * @param {BinaryNode} x
+     * @param {Function} callback
+     */
+    travPost(x, callback) {
+        if (!x) {
+            return;
+        }
+        this.travPost(x.lChild, callback);
+        this.travPost(x.rChild, callback);
+        callback(x);
+    }
+}
+
+
+export {BinaryTree}
