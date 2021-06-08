0b548e696eb41d7e1ddefa20c642d68ffa825388
12212021 <12212021@bjtu.edu.cn>
2021-06-08 12:02:38 +0800

    二分查找update

--- src/algorithm/binarySearch.js
+++ src/algorithm/binarySearch.js
@@ -1,22 +1,82 @@
 /**
- * 二分查找,二分搜索区域减少一半，log(n)（n代表集合中元素的个数）
- * @param {Array} array sorted array
- * @returns {Number} if matched return index of metched element else -1
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} lo
+ * @param {number} hi
  */
-function binarySearch(array, val, begin = 0, end = array.length) {
-    if (end - begin <= 0) {
-        return -1;
+function binarySearchA(nums, target, lo, hi) {
+    while (lo < hi) {
+        const mid = (lo + hi) >> 1;
+        const ele = nums[mid];
+        if (ele === target) {
+            return mid;
+        } else if (ele > target) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
     }
-    const mid = Math.ceil((begin + end) / 2);
-    const midVal = array[mid - 1];
-    if (midVal === val) {
-        return mid - 1;
-    } else if (midVal > val) {
-        return binarySearch(array, val, begin, mid - 1);
-    } else {
-        return binarySearch(array, val, mid, end);
-    }
+    return -1;
 }
 
+/**
+ * 斐波那契近乎黄金分割比例，能从常系数上减少复杂度
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} lo
+ * @param {number} hi
+ */
+function binarySearchB(nums, target, lo, hi) {
+    while (lo < hi) {
+        const mid = Math.floor((lo + hi * 2) / 3);
+        const ele = nums[mid];
+        if (ele === target) {
+            return mid;
+        } else if (ele > target) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    return -1;
+}
 
-export {binarySearch};
+/**
+ * 针对最坏查找情况，能略微提高运算效率
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} lo
+ * @param {number} hi
+ */
+function binarySearchC(nums, target, lo, hi) {
+    while (hi - lo > 1) {
+        const mid = (lo + hi) >> 1;
+        if (nums[mid] < target) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    }
+    return nums[hi] === target ? hi : -1;
+}
+
+/**
+ * 该版本的二分查找能找到nums中值为target且秩最大者
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} lo
+ * @param {number} hi
+ */
+function binarySearchD(nums, target, lo, hi) {
+    while (lo < hi) {
+        const mid = (lo + hi) >> 1;
+        if (nums[mid] <= target) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    }
+    const index = lo - 1;
+    return nums[index] === target ? index : -1;
+}
