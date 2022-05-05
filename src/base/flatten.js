86eb219a6fa8b330a6d0587bca2735f487cd5f3a
weijianghua001 <weijianghua001@ke.com>
2022-05-05 11:38:06 +0800

    代码format

--- src/base/flatten.js
+++ src/base/flatten.js
@@ -1,7 +1,7 @@
 /**
- * 
- * @param {Array} array 
- * @param {Number} deep 
+ *
+ * @param {Array} array
+ * @param {Number} deep
  * @returns a array is flatten in deep
  */
 function flat(array, deep = 1) {
@@ -11,16 +11,16 @@ function flat(array, deep = 1) {
     }
     return arr.reduce((pre, cur) => {
         if (Array.isArray(cur)) {
-            return [...pre, ...flat(cur, deep - 1)]
+            return [...pre, ...flat(cur, deep - 1)];
         }
         return [...pre, cur];
-    }, [])
+    }, []);
 }
 
 /**
- * 
- * @param {Array} array 
- * @param {Number} deep 
+ *
+ * @param {Array} array
+ * @param {Number} deep
  * @returns {Array}
  */
 function flatIter(array, deep = 1) {
@@ -28,11 +28,11 @@ function flatIter(array, deep = 1) {
     let copy = array.slice();
     let result = [];
     // 单调性体现
-    while(++count < deep) {
+    while (++count < deep) {
         let isFlatted = true;
         let index = -1;
         result = [];
-        while(++index < copy.length) {
+        while (++index < copy.length) {
             const val = copy[index];
             if (Array.isArray(val)) {
                 // 单调性体现
